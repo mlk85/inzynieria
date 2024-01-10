6c006a117871cf0582a35a6be5cf5e42c27e78b5
Hubert Malak <123549@uksw.edu.pl>
2024-01-10 16:08:52 +0100

    2nd commit

--- Warsztat.cpp
+++ Warsztat.cpp
@@ -1,128 +1,157 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
-struct DaneSamochodu {
-	string rejestracja;
-	string VIN;
-	string typNadwozia;
-};
-
-struct DaneKlienta {
-	string imie;
-	string nazwisko;
-	int pesel;
-	string adres;
-	int nr_tel;
-	DaneSamochodu samochod;
-};
-
-class Warsztat {
+// Klasa Samochod
+class Samochod {
 private:
-	DaneKlienta * listaKlientow;
-	int liczbaKlientow;
+	string rejestracja;
+	string vin;
+	string typNadwozia;
 
 public:
-	Warsztat() : listaKlientow(nullptr), liczbaKlientow(0) {}
+	Samochod() {}
+	Samochod(const string& rejestracja, const string& vin, const string& typNadwozia)
+		: rejestracja(rejestracja), vin(vin), typNadwozia(typNadwozia) {}
 
-	~Warsztat() {
-		delete[] listaKlientow;
-	}
+	string getRejestracja() const { return rejestracja; }
+	string getVin() const { return vin; }
+	string getTypNadwozia() const { return typNadwozia; }
+};
 
-	void noweZgloszenie() {
-		DaneKlienta nowyKlient;
-
-		cout << "Imie: ";
-		cin >> nowyKlient.imie;
-
-		cout << "Nazwisko: ";
-		cin >> nowyKlient.nazwisko;
-
-		cout << "PESEL: ";
-		cin >> nowyKlient.pesel;
-
-		cout << "Adres: ";
-		cin >> nowyKlient.adres;
-
-		cout << "Nr telefonu: ";
-		cin >> nowyKlient.nr_tel;
-
-		cout << "Rejestracja samochodu: ";
-		cin >> nowyKlient.samochod.rejestracja;
-
-		cout << "VIN samochodu: ";
-		cin >> nowyKlient.samochod.VIN;
-
-		cout << "Typ nadwozia samochodu: ";
-		cin >> nowyKlient.samochod.typNadwozia;
-
-		// Dodaj nowego klienta do listy
-		dodajKlienta(nowyKlient);
-
-		cout << "Nowe zgloszenie dodane." << endl;
-	}
-
-	void serwis() {
-		//serwis
-		cout << "Serwis w toku..." << endl;
-	}
-
-	void wystawienieFaktury() {
-		//wystawiania faktury
-		cout << "Faktura wystawiona." << endl;
-	}
-
+// Klasa Klient
+class Klient {
 private:
-	void dodajKlienta(DaneKlienta nowyKlient) {
-		DaneKlienta* nowaLista = new DaneKlienta[liczbaKlientow + 1];
+	string pesel;
+	string imie;
+	string nazwisko;
+	string adres;
+	string nrTelefonu;
+	Samochod samochod;
 
-		for (int i = 0; i < liczbaKlientow; ++i) {
-			nowaLista[i] = listaKlientow[i];
-		}
+public:
+	Klient() {}
+	Klient(const string& pesel, const string& imie, const string& nazwisko,
+		const string& adres, const string& nrTelefonu, const Samochod& samochod)
+		: pesel(pesel), imie(imie), nazwisko(nazwisko), adres(adres), nrTelefonu(nrTelefonu), samochod(samochod) {}
 
-		nowaLista[liczbaKlientow] = nowyKlient;
+	string getPesel() const { return pesel; }
+	string getImie() const { return imie; }
+	string getNazwisko() const { return nazwisko; }
+	string getAdres() const { return adres; }
+	string getNrTelefonu() const { return nrTelefonu; }
+	Samochod getSamochod() const { return samochod; }
 
-		delete[] listaKlientow;
-		listaKlientow = nowaLista;
-
-		++liczbaKlientow;
+	void wczytajDane() {
+		cout << "Podaj PESEL: ";
+		cin >> pesel;
+		cout << "Podaj imie: ";
+		cin >> imie;
+		cout << "Podaj nazwisko: ";
+		cin >> nazwisko;
+		cout << "Podaj adres: ";
+		cin.ignore(); // Ignoruje pozosta³y bufor
+		getline(cin, adres);
+		cout << "Podaj numer telefonu: ";
+		cin >> nrTelefonu;
 	}
+
+	void zapiszDoPliku(const string& nazwaPliku) {
+		ofstream plik(nazwaPliku, ios::app);
+		if (plik.is_open()) {
+			plik << "PESEL: " << pesel << "\n"
+				<< "Imie: " << imie << "\n"
+				<< "Nazwisko: " << nazwisko << "\n"
+				<< "Adres: " << adres << "\n"
+				<< "Nr telefonu: " << nrTelefonu << "\n\n";
+			plik.close();
+		}
+		else {
+			cerr << "Nie mozna otworzyc pliku do zapisu." << endl;
+		}
+	}
+};
+// Klasa Zgloszenie
+class Zgloszenie {
+private:
+	Klient klient;
+	string opisProblemu;
+
+public:
+	Zgloszenie(const Klient& klient, const string& opisProblemu)
+		: klient(klient), opisProblemu(opisProblemu) {}
+
+	Klient getKlient() const { return klient; }
+	string getOpisProblemu() const { return opisProblemu; }
+};
+
+// Klasa Us³uga
+class Usluga {
+public:
+	virtual void wykonaj() = 0;
+	virtual ~Usluga() {}
+};
+
+class Przeglad : public Usluga {
+public:
+	void wykonaj() override {
+		cout << "Wykonano przeglad." << endl;
+	}
+};
+
+class Naprawa : public Usluga {
+public:
+	void wykonaj() override {
+		cout << "Wykonano naprawe." << endl;
+	}
+};
+
+class Faktura : public Usluga {
+public:
+	void wykonaj() override {
+		cout << "Wystawiono fakture." << endl;
+	}
+};
+
+void wykonajUsluge(Usluga& usluga) {
+	usluga.wykonaj();
+}
+
+// Klasa SerwisSamochodowy
+class SerwisSamochodowy {
+private:
+	vector<Klient> klienci;
+	vector<Zgloszenie> zgloszenia;
+	map<string, Klient*> bazaKlientow;
+
+public:
+	void dodajKlienta(const Klient& klient) {
+		if (bazaKlientow.find(klient.getPesel()) == bazaKlientow.end()) {
+			klienci.push_back(klient);
+			bazaKlientow[klient.getPesel()] = &klienci.back();
+		}
+	}
+
+	void dodajZgloszenie(const Zgloszenie& zgloszenie) {
+		zgloszenia.push_back(zgloszenie);
+	}
+
+	void wykonajUsluge(Usluga& usluga) {
+		usluga.wykonaj();
+	}
+
 };
 
 int main() {
-	Warsztat warsztat;
+	Klient klient;
+	klient.wczytajDane();
+	klient.zapiszDoPliku("dane_klientow.txt");
+	
+	return 0;
 
-	int wybor;
-
-	do {
-		cout << "Menu:" << endl;
-		cout << "1. Nowe zgloszenie" << endl;
-		cout << "2. Serwis" << endl;
-		cout << "3. Wystawienie faktury" << endl;
-		cout << "0. Wyjdz" << endl;
-		cout << "Wybierz opcje: ";
-		cin >> wybor;
-
-		switch (wybor) {
-		case 1:
-			warsztat.noweZgloszenie();
-			break;
-		case 2:
-			warsztat.serwis();
-			break;
-		case 3:
-			warsztat.wystawienieFaktury();
-			break;
-		case 0:
-			cout << "Zakonczono program." << endl;
-			break;
-		default:
-			cout << "Nieprawidlowa opcja. Sprobuj ponownie." << endl;
-		}
-
-	} while (wybor != 0);
-
-	system("pause");
 	return 0;
 }
